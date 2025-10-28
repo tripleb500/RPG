fd609d22a7dd69008758d323fc8b880c10cd736f
tripleb500 <tripleb500@gmail.com>
2025-10-28 16:55:50 -0700

    one achievement posted manually

--- RPG App/RPG/app/src/main/java/com/example/rpg/ui/child/achievements/ChildAchievementsDialog.kt
+++ RPG App/RPG/app/src/main/java/com/example/rpg/ui/child/achievements/ChildAchievementsDialog.kt
@@ -1,5 +1,7 @@
 package com.example.rpg.ui.child.achievements
 
+import android.icu.text.DateFormat
+import android.icu.text.SimpleDateFormat
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +9,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +20,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -24,8 +29,20 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.example.rpg.data.model.Quest
+import com.example.rpg.data.model.Reward
 import com.example.rpg.ui.auth.AuthViewModel
 import com.example.rpg.ui.child.home.ChildHomeScreenViewModel
+import com.example.rpg.ui.child.home.questList
+import com.example.rpg.ui.child.quest.CardView
+import com.example.rpg.ui.child.achievements.achievementsItems
+import java.util.Date
+
+val sdf = SimpleDateFormat("dd/M/yyyy")
+val dateAch = sdf.format(Date())
+val AchievementList = mutableStateListOf(
+    achievementsItems("Go Getter", dateAch, "Get 10 Quests Completed"),
+)
 
 @Composable
 fun ChildAchievementsDialog(
@@ -58,14 +75,22 @@ fun ChildAchievementsDialog(
                     modifier = Modifier.fillMaxWidth(),
                     style = MaterialTheme.typography.titleLarge
                 )
-                OutlinedTextField(
-                    value = username,
-                    onValueChange = { username = it },
-                    label = { Text("Child Username") },
-                    singleLine = true,
-                    modifier = Modifier.fillMaxWidth(),
-                    enabled = !isLoadingAchievements
-                )
+                LazyColumn {
+                    items(AchievementList) { Achievement ->
+                        Text(
+                            text = Achievement.achName,
+                            modifier = Modifier.padding(top = 16.dp)
+                        )
+                        Text(
+                            text = "Earned on: " + Achievement.achDate,
+                            style = MaterialTheme.typography.bodySmall,
+                        )
+                        Text(
+                            text = Achievement.achDesc,
+                            style = MaterialTheme.typography.bodySmall,
+                        )
+                    }
+                }
                 if (errorMessagesAchievements != null) {
                     Text(
                         text = errorMessagesAchievements ?: "",
@@ -85,7 +110,7 @@ fun ChildAchievementsDialog(
                     horizontalArrangement = Arrangement.End
                 ) {
                     TextButton(onClick = { onDismissRequest() }) {
-                        Text("Cancel")
+                        Text("Close")
                     }
                     Spacer(modifier = Modifier.width(8.dp))
 
