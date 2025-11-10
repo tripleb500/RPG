0b955c92e02f4b156966369b038652203aa7bb08
tripleb500 <tripleb500@gmail.com>
2025-11-09 22:17:51 -0800

    stat repo and datasource changes

--- RPG App/RPG/app/src/main/java/com/example/rpg/data/datasource/StatsRemoteDataSource.kt
+++ RPG App/RPG/app/src/main/java/com/example/rpg/data/datasource/StatsRemoteDataSource.kt
@@ -1,7 +1,13 @@
 package com.example.rpg.data.datasource
 
+import com.example.rpg.data.datasource.QuestRemoteDataSource.Companion.QUEST_ITEMS_COLLECTION
+import com.example.rpg.data.model.Quest
 import com.example.rpg.data.model.Stats
+import com.example.rpg.data.model.Status
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.dataObjects
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
@@ -12,7 +18,23 @@ class StatsRemoteDataSource @Inject constructor(
         return firestore.collection(STAT_ITEMS_COLLECTION).add(statItem).await().id
     }
 
+    suspend fun getStats(currentUserIdFlow: Flow<String?>): Flow<List<Stats>> {
+        return currentUserIdFlow.flatMapLatest { childId ->
+            firestore
+                .collection(STAT_ITEMS_COLLECTION)
+                .whereEqualTo("children Id", childId)
+                .dataObjects()
+        }
+    }
+    suspend fun updateStats(statItem: String, newCount: Stats) {
+        firestore.collection(STAT_ITEMS_COLLECTION)
+            .document(statItem)
+            .update("questsCompleted", newCount)
+            .await()
+
+    }
+
     companion object {
-        private const val STAT_ITEMS_COLLECTION = "stats" //Name of the collection for quest items
+        private const val STAT_ITEMS_COLLECTION = "statItem" //Name of the collection for quest items
     }
 }
--- RPG App/RPG/app/src/main/java/com/example/rpg/data/repository/StatsRepository.kt
+++ RPG App/RPG/app/src/main/java/com/example/rpg/data/repository/StatsRepository.kt
@@ -3,13 +3,15 @@ package com.example.rpg.data.repository
 import com.example.rpg.data.datasource.StatsRemoteDataSource
 import com.example.rpg.data.model.Stats
 import jakarta.inject.Inject
+import kotlinx.coroutines.flow.Flow
 
 class StatsRepository @Inject constructor(
     private val statsRemoteDataSource: StatsRemoteDataSource
 ) {
 
     suspend fun createStats(statItem: Stats) = statsRemoteDataSource.createStats(statItem)
-    suspend fun getStats(statItem: Stats) = statsRemoteDataSource.getStats(statItems)
+    suspend fun getStats(currentUserIdFlow: Flow<String?>) = statsRemoteDataSource.getStats(currentUserIdFlow)
+    suspend fun updateStats(statItem: String, newCount: Stats) = statsRemoteDataSource.updateStats(statItem, newCount)
 
 
 }
