3f3243a20e4a6780669dc6480a36a980dea117e3
tripleb500 <tripleb500@gmail.com>
2025-11-11 20:40:39 -0800

    ui for achievements and stats and some logic

--- RPG App/RPG/app/src/main/java/com/example/rpg/ui/child/achievements/ChildAchievementsDialog.kt
+++ RPG App/RPG/app/src/main/java/com/example/rpg/ui/child/achievements/ChildAchievementsDialog.kt
@@ -1,8 +1,9 @@
 package com.example.rpg.ui.child.achievements
 
-import android.icu.text.DateFormat
 import android.icu.text.SimpleDateFormat
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,42 +15,43 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import com.example.rpg.data.model.Quest
-import com.example.rpg.data.model.Reward
 import com.example.rpg.ui.auth.AuthViewModel
 import com.example.rpg.ui.child.home.ChildHomeScreenViewModel
-import com.example.rpg.ui.child.quest.CardView
-import com.example.rpg.ui.child.achievements.achievementItems
 import java.util.Date
 
 val sdf = SimpleDateFormat("dd/M/yyyy")
 val dateAch = sdf.format(Date())
-val AchievementsList = mutableStateListOf(
-    achievementItems("Go Getter", dateAch, "Get 10 Quests Completed"),
-    achievementItems("Go Getter2", dateAch, "Get 20 Quests Completed"),
-    achievementItems("Go Getter3", dateAch, "Get 30 Quests Completed"),
-    achievementItems("Go Getter4", dateAch, "Get 40 Quests Completed"),
-    achievementItems("Go Getter5", dateAch, "Get 50 Quests Completed"),
-    achievementItems("Level Up!!!", dateAch, "Reach level 2"),
-    achievementItems("Level Up2!!!", dateAch, "Reach level 3"),
-    achievementItems("Level Up3!!!", dateAch, "Reach level 4"),
-    achievementItems("Level Up4!!!", dateAch, "Reach level 5"),
-    achievementItems("Level Up5!!!", dateAch, "Reach level 6"),
+val questachievements = mutableStateListOf(
+    questAchievements("Go Getter", dateAch, "Get 1 Quests Completed", 1),
+    questAchievements("Go Getter2", dateAch, "Get 10 Quests Completed", 10),
+    questAchievements("Go Getter3", dateAch, "Get 20 Quests Completed", 20),
+    questAchievements("Go Getter4", dateAch, "Get 30 Quests Completed", 30),
+    questAchievements("Go Getter5", dateAch, "Get 40 Quests Completed", 40),
+)
+val levelachievements = mutableStateListOf(
+    levelAchievements("Level Up!!!", dateAch, "Reach level 2", 2),
+    levelAchievements("Level Up2!!!", dateAch, "Reach level 3", 3),
+    levelAchievements("Level Up3!!!", dateAch, "Reach level 4", 4),
+    levelAchievements("Level Up4!!!", dateAch, "Reach level 5", 5),
+    levelAchievements("Level Up5!!!", dateAch, "Reach level 6", 6),
 
     )
 
@@ -63,8 +65,7 @@ fun ChildAchievementsDialog(
     val isLoadingAchievements by remember { derivedStateOf { viewModel.isLoadingAchievements } }
     val errorMessagesAchievements by remember { derivedStateOf { viewModel.errorMessageAchievements } }
 
-    var username by remember { mutableStateOf("") }
-
+    val count by viewModel.completedQuestsCount.collectAsState()
     Dialog(onDismissRequest = { onDismissRequest() }) {
         Card(
             modifier = Modifier
@@ -81,23 +82,77 @@ fun ChildAchievementsDialog(
                 Text(
                     text = "Achievements",
                     textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
                     modifier = Modifier.fillMaxWidth(),
                     style = MaterialTheme.typography.titleLarge
                 )
+
                 LazyColumn {
-                    items(AchievementsList) { Achievement ->
-                        Text(
-                            text = Achievement.achName,
-                            modifier = Modifier.padding(top = 16.dp)
-                        )
-                        Text(
-                            text = "Earned on: " + Achievement.achDate,
-                            style = MaterialTheme.typography.bodySmall,
-                        )
-                        Text(
-                            text = Achievement.achDesc,
-                            style = MaterialTheme.typography.bodySmall,
-                        )
+                    items(questachievements) { Achievement ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(
+                                    color = if (count > Achievement.achAmount) Color(0xFF6c7d5f) else Color(
+                                        0xFFBBDEFB
+                                    ),
+                                    shape = RoundedCornerShape(12.dp)
+                                )
+                                .padding(vertical = 8.dp)
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                                    ){
+                                Text(
+                                    text = Achievement.achName,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                                Text(
+                                    text = "Earned on: " + Achievement.achDate,
+                                    style = MaterialTheme.typography.bodySmall,
+                                )
+                                Text(
+                                    text = Achievement.achDesc,
+                                    style = MaterialTheme.typography.bodySmall,
+                                )
+                            }
+                        }
+                    }
+                }
+                LazyColumn {
+                    items(levelachievements) { Achievement ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(
+                                    color = if (count > Achievement.achAmount) Color(0xFF6c7d5f) else Color(
+                                        0xFFBBDEFB
+                                    ),
+                                    shape = RoundedCornerShape(12.dp)
+                                )
+                                .padding(vertical = 8.dp)
+                        ) {
+                            Column (
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ){
+                                Text(
+                                    text = Achievement.achName,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                                Text(
+                                    text = "Earned on: " + Achievement.achDate,
+                                    style = MaterialTheme.typography.bodySmall,
+                                )
+                                Text(
+                                    text = Achievement.achDesc,
+                                    style = MaterialTheme.typography.bodySmall,
+                                )
+                            }
+                        }
                     }
                 }
                 if (errorMessagesAchievements != null) {
--- RPG App/RPG/app/src/main/java/com/example/rpg/ui/child/achievements/achievementsItems.kt
+++ RPG App/RPG/app/src/main/java/com/example/rpg/ui/child/achievements/achievementsItems.kt
@@ -1,10 +1,16 @@
 package com.example.rpg.ui.child.achievements
 
-import java.util.Date
-
-data class achievementItems(
+data class questAchievements(
     val achName: String,
     val achDate: String, //date it was gotten
-    val achDesc: String //description of achievement
+    val achDesc: String, //description of achievement
+    val achAmount: Int
 
+)
+
+data class levelAchievements(
+    val achName: String,
+    val achDate: String,
+    val achDesc: String,
+    val achAmount: Int
 )
