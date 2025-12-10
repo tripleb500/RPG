7444ab740fc1478e0804c087589750fab4a37093
Erick-28 <80983678+Erick-28@users.noreply.github.com>
2025-12-10 12:09:58 -0800

    Improved Navigation flow on parent side settings. Back Arrow implemented on settings pages to improve flow.

--- RPG App/RPG/app/src/main/java/com/example/rpg/ui/parent/settings/ParentAccountSettingsScreen.kt
+++ RPG App/RPG/app/src/main/java/com/example/rpg/ui/parent/settings/ParentAccountSettingsScreen.kt
@@ -6,8 +6,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -44,6 +49,20 @@ fun ParentAccountSettingsScreen(
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Color(0xFF1B2631)
                 ),
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.navigate(Routes.ParentSettingsScreen.route) {
+                                popUpTo(Routes.ParentSettingsScreen.route) { saveState = true }
+                                launchSingleTop = true
+                            }
+                        }
+                    ) {
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White)
+                    }
+                },
                 title = {
                     Text(
                         "Account Settings",
--- RPG App/RPG/app/src/main/java/com/example/rpg/ui/parent/settings/ParentSettingsScreen.kt
+++ RPG App/RPG/app/src/main/java/com/example/rpg/ui/parent/settings/ParentSettingsScreen.kt
@@ -14,9 +14,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -55,7 +59,16 @@ fun ParentSettingsScreen(
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Color(0xFF1B2631)
                 ),
-
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.popBackStack()
+                        }
+                    ) {
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White)
+                    }
+                },
                 title = {
                     Text(
                         "Settings",
